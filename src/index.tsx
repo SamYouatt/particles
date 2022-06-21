c5f1527c6ef90de6d3a37e71890dc3d95759b5b7
Sam Youatt <sdyouatt@btinternet.com>
2022-06-21 09:19:07 +0100

    added webasm output

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import init from './out/particles-game.js';
+import init from './webasm-particles/particles-game.js';
 
 init();
 
